bafff34eb46e295ef794839e1f2a919ac767aa71
Charlie Huang <hyichao@foxmail.com>
2016-09-21 09:19:01 +0800

    add header for general import

---
+++ CrazyImage/Classes/CrazyImage.h
@@ -0,0 +1,19 @@
+//
+//  CrazyImage.h
+//  Pods
+//
+//  Created by HuangCharlie on 9/21/16.
+//
+//
+
+#ifndef CrazyImage_h
+#define CrazyImage_h
+
+
+#import "UIImage+Buffer.h"
+#import "UIImage+CharArray.h"
+
+//Tool
+#import "ScopeTimer.h"
+
+#endif /* CrazyImage_h */
